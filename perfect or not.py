1e06d94bbd3dc9b75f38661f120354cc47a1ba88
sharmilaKcse <66429293+sharmilaKcse@users.noreply.github.com>
2020-11-27 07:56:21 -0800

    Add files via upload

---
+++ perfect or not.py
@@ -0,0 +1,18 @@
+n=int(input("Enter the number:"))
+result=0
+for i in range(1,n):
+    if(n%i)==0:
+        result=result+i
+if result==n:
+    print(n,"is a perfect number")
+else:
+    print(n,"is not a perfect number")
+
+
+
+
+
+
+
+
+
